23ee5b9603bb8e88dcebdedc2298f95d62dc5042
AthanasiosDelis <th.delis@gmail.com>
2022-04-20 23:44:56 +0300

    Update
    Generalized two_files for both 1st and 2nd project needs. Plus some .hdl

--- support_scripts/two_files_same_content.py
+++ support_scripts/two_files_same_content.py
@@ -8,17 +8,18 @@ Created on Tue Apr 19 19:47:01 2022
 #!/usr/bin/env python3
 import sys
 from os import listdir
-from os.path import isfile, join
 
 def readfile(path):
     with open(path, 'rb') as f:
         return f.read()
-mypath = r'C:\Users\Thanasis\Documents\nand2tetris\nand2tetris\projects\01'
+mypath = r'C:\Users\Thanasis\Documents\nand2tetris\nand2tetris\projects\02'
 contentscmp = []
 contentsout = []
-contentscmp = [(mypath+r'\\'+fname) for fname in listdir(mypath) if ('.cmp' in fname )]
-contentsout = [(mypath+r'\\'+fname) for fname in listdir(mypath) if ( '.out' in fname)]
-print(len(contentscmp),len(contentsout), sep = '\n')
-#print(, sep = '\n')
-sys.exit(all([readfile(contentout)==readfile(contentcmp) \
-       for contentout,contentcmp in zip(contentscmp,contentsout)]))
+contentscmp = [(mypath+'\\'+fname) for fname in listdir(mypath) if ('.cmp' in fname )]
+contentsout = [(mypath+'\\'+fname) for fname in listdir(mypath) if ( '.out' in fname)]
+names = [ fname.split('.')[0] for fname in contentsout  ]
+#print(len(contentscmp),len(contentsout), sep = '\n')
+print(* [readfile(name + '.cmp')==readfile(name + '.out') \
+       for name in names ], sep = '\n')
+sys.exit(all([readfile(name + '.cmp')==readfile(name + '.out') \
+       for name in names ]))
